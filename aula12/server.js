27c6fd5cc0eb38fcc7dab6080ddf230b99eed9c3
Eduardo <eduardo.munhoz97@outlook.com>
2024-01-03 20:59:26 -0300

    Unindo Express com Webpack

---
+++ aula12/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const route = require('./routes')
+const path = require('path')
+//CRUD -> CREATE(POST), READ(GET), UPDATE(PUT), DELETE(DELETE)
+
+app.use(express.urlencoded( { extended: true} ))
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
+
+app.use(route)
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000');
+  console.log('Servidor rodando.');
+});
